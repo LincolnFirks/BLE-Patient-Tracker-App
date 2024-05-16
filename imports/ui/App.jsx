f02c375194ab1410430dac42de6285dbd18121b7
LincolnFirks <lincfirks@gmail.com>
2024-05-16 15:13:19 -0400

    first commit

---
+++ imports/ui/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { useTracker } from 'meteor/react-meteor-data';
+import { TasksCollection } from '/imports/api/TasksCollection';
+
+
+export const App = () => {
+  const tasks = useTracker(() => TasksCollection.find().fetch());
+
+  return (
+    <div>
+      <h1>Welcome to Meteor!!</h1>
+
+      <ul>
+        {tasks.map(task => (
+          <li> {task.entry}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
