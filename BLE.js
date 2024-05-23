5666359264d8833a4db6dfd44e45c5e3d9f728a4
lincolnfirks <lincolnfirks@gmail.com>
2024-05-23 15:57:54 -0400

    implement json and sim

--- BLE.js
+++ BLE.js
@@ -1,124 +1,52 @@
 const BeaconScanner = require('node-beacon-scanner');
 const { update, client } = require("./update");
 const { parse } = require('path');
+const fs = require("fs");
+const getMAC = require("getmac").default;
 
+let beaconData = JSON.parse(fs.readFileSync("beacons.json", "utf-8"));
 
 const scanner = new BeaconScanner();
 
+getMAC((err, macAddress) => {
+  if (err) {
+    console.error("Error retrieving MAC address:", err);
+  } else {
+    const localMac = macAddress;
+  }
+});
 
-const beacon1 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D615',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon1"
-}
-
-const beacon2 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D616',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon2"
-}
-
-const beacon3 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D617',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon3"
-}
-
-const beacon4 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D618',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon4"
-}
-
-const beacon5 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D619',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon5"
-}
-
-const beacon6 = {
-  identifiers: {
-    uuid: '6B9FFDFF-5A95-4379-9833-08A0FD76D610',
-    major: 100,
-    minor: 6
-  },
-  status: false,
-  distanceReadings: [], // collection of last 5 readings
-  collection: "beacon6"
-}
 
 // Handle beacon advertisements
 scanner.onadvertisement = (ad) => {
-  HandleAd(ad);
+  console.log(ad)
+  HandleAd(ad, new Date());
 };
 
-function HandleAd(ad) {
-  exponent = (ad.iBeacon.txPower - ad.rssi) / (10 * 2);
-  distance = Math.pow(10, exponent) * 3.28; // convert to feet
+function HandleAd(ad, time) {
+  let exponent = (ad.iBeacon.txPower - ad.rssi) / (10 * 2);
+  let distance = Math.pow(10, exponent) * 3.28; // convert to feet
   distance = parseFloat(distance.toFixed(1)); // this is est. feet from beacon
-  let now = new Date(); // fetch current exact date/time
-  const formattedDate = now.toLocaleDateString('en-US');
-  const formattedTime = now.toLocaleTimeString('en-US');
-  now = formattedDate + " " + formattedTime;
-  let device = { 
-    identifiers: { 
-      uuid: ad.iBeacon.uuid,
-      major: ad.iBeacon.major,
-      minor: ad.iBeacon.minor,
-    },
-    distance: distance,
-    time: now,
+
+  const matchingBeacon = beaconData.beacons.find(beacon => beacon.address === ad.address);
+
+  if (matchingBeacon) {
+    HandleUpdate(matchingBeacon, distance, time)
   }
   
-  if (device.identifiers.uuid === beacon1.identifiers.uuid) HandleUpdate(device, beacon1);
-  if (device.identifiers.uuid === beacon2.identifiers.uuid) HandleUpdate(device, beacon2);
-  if (device.identifiers.uuid === beacon3.identifiers.uuid) HandleUpdate(device, beacon3);
-  if (device.identifiers.uuid === beacon4.identifiers.uuid) HandleUpdate(device, beacon4);
-  if (device.identifiers.uuid === beacon5.identifiers.uuid) HandleUpdate(device, beacon5);
-  if (device.identifiers.uuid === beacon6.identifiers.uuid) HandleUpdate(device, beacon6);
-  
 };
 
-function HandleUpdate(device, beacon) {
-  beacon.distanceReadings.push(device.distance); // add distance reading to array
+function HandleUpdate(beacon, distance, time) {
+  beacon.distanceReadings.push(distance); // add distance reading to array
+
   if (beacon.distanceReadings.length < 5) {
     return; //  if less than 5, keep collecting
   } // otherwise:
-  console.log(average(beacon.distanceReadings))
-  console.log((beacon.distanceReadings))
-  if ((average(beacon.distanceReadings) < 20) && (beacon.status == false)) {
-    update("meteor",beacon.collection, true, device.time);
-    beacon.status = true;
-    // if within 20 feet and current location is outside, send update and change status
-  } else if ((average(beacon.distanceReadings) >= 20) && (beacon.status == true)) {
-    update("meteor", beacon.collection, false, device.time);
-    beacon.status = false;
-  } // if outside 20 feet and current location is inside, send update and change status
+ 
+  if (average(beacon.distanceReadings) < 30) {
+    update(beacon, time, localMac);
+    // if within 20 feet and current location is outside, send update 
+  } 
   beacon.distanceReadings = [];
 }
 // connect to MongoDB server and start scanning for BLE advertisements.
--- update.js
+++ update.js
@@ -1,10 +1,12 @@
 const { MongoClient, ServerApiVersion } = require("mongodb");
+const fs = require("fs");
+let config = JSON.parse(fs.readFileSync("config.json", "utf-8"));
+
 
 // Replace the placeholder with your Atlas connection string
-const uri = "mongodb://127.0.0.1:3001/";
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-const client = new MongoClient(uri,  {
+const client = new MongoClient(config.serverURL,  {
         serverApi: {
             version: ServerApiVersion.v1,
             strict: true,
@@ -14,23 +16,18 @@ const client = new MongoClient(uri,  {
 );
 // takes device data, and database, collection to  insert into
 // is status true, device entered area. If false, it left.
-function update(database, beaconName, status, time) {
-  const myDB = client.db(database);
-  const myColl = myDB.collection(beaconName);
-  let entry;
-  if (status === true) {
-    entry = { beaconName,
-              status: "Entered",
-              time,
-          };
-  } else {
-    entry = { beaconName,
-              status: "Left",
-              time,
-          };
+function update(beacon, time, location) {
+  const myDB = client.db(config.database);
+  const myColl = myDB.collection(beacon.ID);
+  let entry = {
+    beaconID: beacon.ID,
+    address: beacon.address,
+    location,
+    time
   }
-  const result = myColl.insertOne(entry);
+  myColl.insertOne(entry);
   console.log(`A document was inserted`);
+  console.log(entry)
 
 }
 
