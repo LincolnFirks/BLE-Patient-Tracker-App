285f9b234a618a6cff3318ea18f2cf9ccaeafdb8
LincolnFirks <lincfirks@gmail.com>
2024-07-19 13:20:36 -0400

    update gitignore

--- app-backend/tests/main.js
+++ app-backend/tests/main.js
@@ -1,9 +1,9 @@
 import assert from "assert";
 
-describe("simple-todos-react", function () {
+describe("BLE-Patient-Tracker-App", function () {
   it("package.json has correct name", async function () {
     const { name } = await import("../package.json");
-    assert.strictEqual(name, "simple-todos-react");
+    assert.strictEqual(name, "BLE-Patient-Tracker-App");
   });
 
   if (Meteor.isClient) {
